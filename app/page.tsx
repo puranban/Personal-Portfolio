e2eb423dd43daeeeab7dcc1e80f64087dbbcd03f
puranban <puranban77@gmail.com>
2024-12-13 10:23:52 +0545

    Setup: initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="py-24">
+      <main className="container">
+        <h1 className="text-3xl font-bold">
+          Homepage
+        </h1>
+      </main>
+    </div>
+  );
+}
